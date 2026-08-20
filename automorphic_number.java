d8ed4b8c7f94b61ce006cd650b48eafd85daa28d
AishwaryaB05 <aishwarya.b_25it@kgkite.ac.in>
2026-08-20 11:21:42 +0530

    Automorphic number code prgrm

---
+++ automorphic_number.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class automorphic_number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int n = sc.nextInt();
+
+        int square = n * n;
+
+        if (square % 10 == n % 10)
+            System.out.println(n + " is an Automorphic Number");
+        else
+            System.out.println(n + " is not an Automorphic Number");
+        sc.close();
+    }
+}
