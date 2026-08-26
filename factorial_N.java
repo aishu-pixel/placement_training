9bc7d0c3a70298b38e13ad1f91f6244caea67f69
AishwaryaB05 <aishwarya.b_25it@kgkite.ac.in>
2026-08-26 13:04:32 +0530

    Factorial of N

---
+++ factorial_N.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class factorial_N {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int n = sc.nextInt();
+
+        int factorial = 1;
+
+        for (int i = 1; i <= n; i++) {
+            factorial = factorial * i;
+        }
+
+        System.out.println("Factorial of " + n + " is: " + factorial);
+        sc.close();
+    }
+}
