6b3c520b4e725339787868a1273be9909d79f6c8
AishwaryaB05 <aishwarya.b_25it@kgkite.ac.in>
2026-08-26 12:53:55 +0530

    Printing 1 to N

---
+++ print_1_N.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class print_1_N {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the value of N:");
+        int n = sc.nextInt();
+
+        System.out.println("Numbers from 1 to " + n + ":");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+        sc.close();
+    }
+}
