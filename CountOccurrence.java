310e76c944319e07cc4e2a58a9b52589354751a5
AishwaryaB05 <aishwarya.b_25it@kgkite.ac.in>
2026-09-16 11:01:01 +0530

    Count the number of occurance of a given number in array

---
+++ CountOccurrence.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class CountOccurrence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] a = new int[rows][cols];
+
+        System.out.println("Enter the elements:");
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.print("Enter the number to count: ");
+        int number = sc.nextInt();
+
+        int count = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (a[i][j] == number) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(number + " occurs " + count + " time(s).");
+
+        sc.close();
+    }
+}
