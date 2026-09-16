3ceaea7113122b2b0252b47144eeb48fa7e96e69
AishwaryaB05 <aishwarya.b_25it@kgkite.ac.in>
2026-09-16 10:38:57 +0530

    Even odd Count of array

---
+++ EvenOddCount.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class EvenOddCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] a = new int[rows][cols];
+        int even = 0;
+        int odd = 0;
+
+        System.out.println("Enter the elements:");
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                a[i][j] = sc.nextInt();
+
+                if (a[i][j] % 2 == 0) {
+                    even++;
+                } else {
+                    odd++;
+                }
+            }
+        }
+
+        System.out.println("Number of even elements = " + even);
+        System.out.println("Number of odd elements = " + odd);
+
+        sc.close();
+    }
+}
